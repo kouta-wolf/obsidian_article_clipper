f76c43ad4e65f31716452db192499f39412dcce0
kouta-wolf <koutaookami18@gmail.com>
2026-04-14 23:02:38 +0900

    test_build_uriを設定

--- obsidian_clipper.rb
+++ obsidian_clipper.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 class ObsidianClipper
   def initialize(vault:)
     @vault = vault
@@ -5,10 +7,19 @@ class ObsidianClipper
 
   def format_content(title, url)
     <<~TEXT.chomp
+    # #{title}
+
     tags: #clip #later
     url: #{url}
 
     - [ ] 読む
     TEXT
+  end
+
+  def build_uri(name, content)
+    encoded_name = CGI.escape(name)
+    encoded_content = CGI.escape(content)
+
+    "obsidian://new?vault=#{@vault}&name=#{encoded_name}&content=#{encoded_content}"
   end
 end
--- test/obsidian_clipper_test.rb
+++ test/obsidian_clipper_test.rb
@@ -11,6 +11,8 @@ class ObsidianClipperTest < Minitest::Test
     title = "Qiitaの記事"
     
     expected = <<~TEXT.chomp
+      # Qiitaの記事
+
       tags: #clip #later
       url: https://qiita.com/example
 
@@ -18,5 +20,15 @@ class ObsidianClipperTest < Minitest::Test
     TEXT
 
     assert_equal expected, @clipper.format_content(title, url)
+  end
+
+  def test_build_uri
+    title = "Rubyの記事"
+    content = "tags: #later"
+
+    uri = @clipper.build_uri("later/#{title}", content)
+
+    assert_match /^obsidian:\/\/new\?vault=my-notes/, uri
+    assert_includes uri, "name=later%2FRuby%E3%81%AE%E8%A8%98%E4%BA%8B"
   end
 end
