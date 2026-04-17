7399c1f52bd88fd9a834a389b5d87f4845235a57
kouta-wolf <koutaookami18@gmail.com>
2026-04-17 20:05:06 +0900

    余計な記述を削除

--- app.rb
+++ app.rb
@@ -36,9 +36,3 @@ get '/clip' do
     "エラーが発生しました: #{e.message}"
   end
 end
-
-clip?url=https%3A%2F%2Fqiita.com%2FNoripy%2Fitems%2F9852f0c6e734c35b4190:1 Launched external handler for 'obsidian://new?vault=obsidian&file=06_clippings%2FRails%E3%81%AE%E9%AB%98%E5%BA%A6%E3%81%AA%E6%A4%9C%E7%B4%A2%E6%A9%9F%E8%83%BDgem%20%60ransack%60%20%E3%82%92%E5%AD%A6%E3%81%B6%20-Gem%20-%20Qiita&content=---%0Atitle%3A%20%22Rails%E3%81%AE%E9%AB%98%E5%BA%A6%E3%81%AA%E6%A4%9C%E7%B4%A2%E6%A9%9F%E8%83%BDgem%20%60ransack%60%20%E3%82%92%E5%AD%A6%E3%81%B6%20%23Gem%20-%20Qiita%22%0Asource%3A%20%22https%3A%2F%2Fqiita.com%2FNoripy%2Fitems%2F9852f0c6e734c35b4190%22%0Aauthor%3A%20%0A-%20Qiita%0Acreated%3A%202026-04-15%0Atags%3A%0A%20%20-%20webclip%0A%20%20-%20unread%0A---%0A%23%20Rails%E3%81%AE%E9%AB%98%E5%BA%A6%E3%81%AA%E6%A4%9C%E7%B4%A2%E6%A9%9F%E8%83%BDgem%20%60ransack%60%20%E3%82%92%E5%AD%A6%E3%81%B6%20%23Gem%20-%20Qiita'.
-clip?url=https%3A%2F%2Fqiita.com%2FNoripy%2Fitems%2F9852f0c6e734c35b4190:6 Scripts may close only the windows that were opened by them.
-（匿名） @ clip?url=https%3A%2F%2Fqiita.com%2FNoripy%2Fitems%2F9852f0c6e734c35b4190:6
-setTimeout
-（匿名） @ clip?url=https%3A%2F%2Fqiita.com%2FNoripy%2Fitems%2F9852f0c6e734c35b4190:6
